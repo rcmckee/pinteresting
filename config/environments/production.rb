02da59da591bc9589013d81bd51b8a3068e31a6f
rcmckee <rmckee8@gmail.com>
2014-07-07 23:26:08 -0700

    implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  
+  # note to set this to your actual host
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
